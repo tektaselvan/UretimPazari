d3d7f34d913be47bf14662e17772a8a2c8a4232a
Elvan Tektaş <62278777+tektaselvan@users.noreply.github.com>
2025-05-08 15:39:17 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UretimPazari.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
