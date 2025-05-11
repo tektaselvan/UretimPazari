07102020583bb792c51471cad1c288845aef656e
Elvan Tektaş <62278777+tektaselvan@users.noreply.github.com>
2025-05-11 18:52:31 +0300

    Delete Models directory

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace UretimPazari.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
