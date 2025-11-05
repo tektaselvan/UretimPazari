0ded5c921dfabfcd655ae174225ae6444b1db916
Elvan Tektaş <62278777+tektaselvan@users.noreply.github.com>
2025-11-05 20:34:25 +0300

    homepage arayüz çalışması yapıldı

--- UretimPazari.Repositories/obj/Debug/net6.0/UretimPazari.Repositories.AssemblyInfo.cs
+++ UretimPazari.Repositories/obj/Debug/net6.0/UretimPazari.Repositories.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UretimPazari.Repositories")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90279da7da0536fca9e200aa9014283b6c18b91e")]
 [assembly: System.Reflection.AssemblyProductAttribute("UretimPazari.Repositories")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UretimPazari.Repositories")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
